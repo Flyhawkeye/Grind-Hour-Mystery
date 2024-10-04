f67386c4c9224220a5f1abb92f99852e631c5c17
Flyhawkeye <65386808+Flyhawkeye@users.noreply.github.com>
2024-10-04 02:00:46 -0500

    Update

---
+++ api.js
@@ -0,0 +1,7 @@
+const axios = require('axios');
+const api = axios.create({
+baseURL: process.env.API_URL || 'http://localhost:3000/api',
+headers: { 'Content-Type': 'application/json' }
+});
+
+module.exports = api;
